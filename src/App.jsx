6c03103cd176f5c93aad170796569630a0c78a81
Ameer Muhammad <amyrmian789@gmail.com>
2026-05-12 18:02:55 +0500

    added navbar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from './assets/components/navbar'
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import './navbar.css'
+function Navbar() {
+    return (
+        <>
+            <div className="card"></div>
+        </>
+    )
+}
+
+export default Navbar;
