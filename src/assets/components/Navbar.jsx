39381e28bcf5610baf110c4b064ba5a3b681bced
Ameer Muhammad <amyrmian789@gmail.com>
2026-05-15 11:27:19 +0500

    navbar styles

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -3,6 +3,10 @@ function Navbar() {
     return (
         <>
             <div className="card">
+                <a href="" className='link'>Ameer Muhammad</a>
+                <a href="" className='link'>About me</a>
+                <a href="" className='link'>Skills</a>
+                <a href="" className='link'>Projects</a>
             </div>
         </>
     )
