6c5412db2bac730f15935ca5fd75fcb5fd933207
Ameer Muhammad <amyrmian789@gmail.com>
2026-05-16 09:19:08 +0500

    character image addes

---
+++ src/assets/components/character/character.jsx
@@ -0,0 +1,11 @@
+import './character.css'
+
+function Character(){
+    return(
+        <div className="img-container">
+            <img src="/src/assets/images/Character.png" alt="Character" />
+        </div>
+    )
+}
+
+export default Character;
--- src/assets/components/hero/Hero.jsx
+++ src/assets/components/hero/Hero.jsx
@@ -1,10 +1,14 @@
 import Navbar from '../navbar/Navbar.jsx'
+import Character from '../character/character.jsx'
 import './hero.css'
+
+
 
 function Hero() {
     return (
         <div className="hero">
             <Navbar />
+            <Character />
         </div>
     )
 }
