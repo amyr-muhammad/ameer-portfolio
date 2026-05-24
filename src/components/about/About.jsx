29a074afb676cfff23be5ea3d8ab9bee1e72aa3d
Ameer Muhammad <amyrmian789@gmail.com>
2026-05-24 15:55:59 +0500

    tech stack added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -17,22 +17,32 @@ function About() {
 
                 </h2>
                 <div className="grid grid-rows-max grid-cols-1 gap-3 p-5 justify-items-center ">
-                    <GlassCard size="large">Hi, I’m Ameer,
+                    <GlassCard size="large">Hi, I’m Ameer, <br />
                         I’m a Web Developer and Designer focused on creating clean visuals and turning them into functional, high performing websites. I enjoy bringing ideas to life through code.</GlassCard>
                     <GlassCard size="medium">
-                        Education: <br />
+                        <b>Education: </b> <br />
                         ICS- Completed Intermediate in Computer
                         Science BSCS- On going Bachelors  in Computer Science (6th Semester)
                     </GlassCard>
                     <GlassCard size="small">
-                        Contact at:
+                        <b>Contact at:</b>
                         <a href="" className="flex justify-start items-center text-#1a1a4c"><BiLogoGmail />: amyrmian789@gmail.com</a>
                         <a href="" className="flex justify-start items-center text-[#1a1a4c]"><FaLinkedin />: Ameer Muhammad</a>
                         <a href="" className="flex justify-start items-center text-[#1a1a4c]">< FaSquareInstagram />: amyr_muhmmad</a>
                     </GlassCard>
-                    <GlassCard size="small">
-                        Tech stack:
-                        
+                    <GlassCard size="medium">
+                        <b className="mb-1">Tech stack:</b>
+                        <div className="grid grid-cols-4 gap-x-2 gap-y-2 justify-items-center w-max mx-auto">
+                            <img src="./src/assets/images/Icons/html5.png" alt="" className="w-13" />
+                            <img src="./src/assets/images/Icons/css.png" alt="" className="w-13" />
+                            <img src="./src/assets/images/Icons/js.png" alt="" className="w-13" />
+                            <img src="./src/assets/images/Icons/reactjs.png" alt="" className="w-13" />
+                            <img src="./src/assets/images/Icons/tailwind.png" alt="" className="w-13" />
+                            <img src="./src/assets/images/Icons/figma.png" alt="" className="w-13" />
+                            <img src="./src/assets/images/Icons/github.png" alt="" className="w-13" />
+                            <img src="./src/assets/images/Icons/vs.png" alt="" className="w-13" />
+
+                        </div>
                     </GlassCard>
 
                     <img
--- src/components/about/Glasscard.jsx
+++ src/components/about/Glasscard.jsx
@@ -3,7 +3,7 @@ function GlassCard({ children, size }) {
     const sizes = {
         small: "w-65 h-30",
         medium: "w-65 h-40",
-        large: "w-65 h-50"
+        large: "w-65 h-52"
     }
     return (
         <div className={`backdrop-blur-[0.5px] backdrop-saturate-162 bg-[rgba(100,100,100,0.35)] rounded-xl border border-[rgba(255,255,255,0.125)] text-[#1a1a4c] ${sizes[size]} ${size === 'small' ? 'p-2' : 'p-3'}`}>
