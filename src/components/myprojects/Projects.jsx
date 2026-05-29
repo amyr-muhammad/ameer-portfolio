faea28b8509201b7e8070ff38f1a41a59e52fe98
Ameer Muhammad <amyrmian789@gmail.com>
2026-05-29 12:03:07 +0500

    files

--- src/components/myprojects/Projects.jsx
+++ src/components/myprojects/Projects.jsx
@@ -1,7 +1,11 @@
 function Projects() {
     return (
         <>
-            <div className="relative w-full min-h-screen bg-[url('/src/assets/images/checkbackground.jpg')] bg-no-repeat bg-cover overflow-hidden">
+            <div className="relative w-full 
+            min-h-screen bg-[url('/src/assets/images/checkbackground.jpg')] 
+            bg-no-repeat bg-cover overflow-hidden">
+
+                <h3 className="text-8xl text-[#1a1a4c] ">Projects I have made</h3>
             
             </div>
         </>
